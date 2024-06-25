feea05ef6a7818fb440de838defb4a19fa4d228e
Scott Olesen <ulp7@cdc.gov>
2024-06-25 10:11:44 -0400

    use __all__

--- cladecombiner/__init__.py
+++ cladecombiner/__init__.py
@@ -1,6 +1,6 @@
-from .nomenclature import PangoSc2Nomenclature as PangoSc2Nomenclature
-from .taxon import Taxon as Taxon
-from .taxon_utils import read_taxa as read_taxa
-from .taxonomy_scheme import (
-    PhylogeneticTaxonomyScheme as PhylogeneticTaxonomyScheme,
-)
+from .nomenclature import PangoSc2Nomenclature
+from .taxon import Taxon
+from .taxon_utils import read_taxa
+from .taxonomy_scheme import PhylogeneticTaxonomyScheme
+
+__all__ = ["PangoSc2Nomenclature", "Taxon", "read_taxa", "PhylogeneticTaxonomyScheme"]
